524d6f88f3b391f1fb16be806ac94b9c8df9646c
linganesan <linganesan.12@cse.mrt.ac.lk>
2016-09-25 01:17:07 +0530

    update CSVWriter

--- src/main/java/com/taz/jfrparser/core/CSVWriter.java
+++ src/main/java/com/taz/jfrparser/core/CSVWriter.java
@@ -3,6 +3,9 @@ package com.taz.jfrparser.core;
 import com.taz.jfrparser.cpuanalyzer.CPULoadEvent;
 import com.taz.jfrparser.gcanalyzer.MemEvent;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 /**
@@ -19,14 +22,26 @@ public class CSVWriter {
     private CSVWriter() {
     }
 
+
     public void printCPUOutput(ArrayList<CPULoadEvent> eventList){
+        File courseCSV = new File("CPULoad.csv");
+
+        //create PrintWriter object on new File object
+        PrintWriter outfile = null;
+        try {
+            outfile = new PrintWriter(courseCSV);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         for(int i=0;i<eventList.size();i++){
             try{
                 System.out.println(eventList.get(i).getEndTimestamp()-eventList.get(i-1).getStartTimestamp()+"  "+eventList.get(i).getJvmUser());
+                outfile.write(eventList.get(i).getEndTimestamp()-eventList.get(i-1).getStartTimestamp()+","+eventList.get(i).getJvmUser() + "\n");
             }catch(IndexOutOfBoundsException e){
 
             }
         }
+        outfile.close();
     }
 
     public void printGCOutput(ArrayList<MemEvent> eventList){
