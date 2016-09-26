d88091fa1700771a497d3e994151876dd5fecb05
linganesan <linganesan.12@cse.mrt.ac.lk>
2016-09-26 23:37:44 +0530

    add GC state output class based on High,low approach

---
+++ src/main/java/com/taz/jfrparser/gcanalyzer/GCTimeSeriesModel.java
@@ -0,0 +1,40 @@
+package com.taz.jfrparser.gcanalyzer;
+
+import com.jrockit.mc.flightrecorder.spi.IView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by  Maninesan on 9/26/16.
+ */
+public class GCTimeSeriesModel {
+    private IView view;
+    private Map<Long,MemEvent> eventMap;
+    private GCTimeHandler gcTimeHandler;
+    private GCMemoryHandler gcMemoryHandler;
+    private StateIdentifier stateIdentifier;
+    private ArrayList<Integer> stateSequence;
+
+    public GCTimeSeriesModel(IView view){
+        this.view = view;
+        eventMap = new LinkedHashMap<Long, MemEvent>();
+
+    }
+
+
+    public ArrayList<Integer> getOuputOne(){
+        gcTimeHandler= new GCTimeHandler(view, eventMap);
+        gcTimeHandler.configureEventGCTime();
+
+        gcMemoryHandler= new GCMemoryHandler(view, eventMap);
+        gcMemoryHandler.configureGCMemory();
+
+        stateIdentifier= new StateIdentifier(eventMap);
+        stateSequence = stateIdentifier.configureStates();
+
+        return stateSequence;
+    }
+}
